99f11f005442191f021d20d4cfae84973ba170ba
Tripple <github@torben.joneit.de>
2024-05-13 20:36:14 +0200

    cleaned

--- input.py
+++ input.py
@@ -16,19 +16,18 @@ def handleOutput(unencrypted, decrypted) :
         print("Failure!")
     
 def binOf(stri) :
-    lst = [ord(char) for char in stri]
-    lst = [bin(intg)[2:] for intg in lst]
-    lst = [missingDigits(bin, charLength) + bin for bin in lst]
-    lst = [''.join(lst[i:i+charpBlock]) for i in range(0, len(lst), charpBlock)]
-    return int(''.join(lst),2)
+    bin = 0
+    for i in range(len(stri)) :
+        bin = (bin << charLength) | ord(stri[i])
+    return bin
 
 def blocksOf(intg) :
     bstr = bin(intg)[2:]
-    bstr = missingDigits(bstr, 64) + bstr
-    bstr = len(bstr)
-
+    bstr = len(missingDigits(bstr, 64) + bstr)
     mask = int('1' * blockLength, 2)
     return [(intg & (mask << i)) >> i for i in range(0, bstr, blockLength)]
 
 def missingDigits(a, b) :
     return "0" * (b - (len(a) % b))
+
+binOf("Hellooo")
